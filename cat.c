fb19e990b7695a986e7d8a8ffc20b3556ede60ea
betim-hodza <bhodza02@gmail.com>
2025-12-26 12:34:40 +0100

    working cat !

--- cat.c
+++ cat.c
@@ -6,7 +6,13 @@
 #define SYS_read 0
 #define SYS_write 1
 #define SYS_open 2
+#define SYS_close 6
 #define SYS_exit 60
+
+// file operations
+#define O_RDONLY 00
+#define O_WRONLY 01
+#define O_RDWR 02
 
 long syscall1(long number, long arg1)
 {
@@ -41,6 +47,8 @@ long syscall3(long number, long arg1, long arg2, long arg3)
 // func prototypes
 long unsigned strlen(char *string);
 long fileopen(char *path, char *mode);
+void fileclose(long fd);
+long getcharacter(long fd, char *ch);
 void print(char *string);
 void putcharacter(char *character);
 void exit(int code);
@@ -66,16 +74,34 @@ long fileopen(char *path, char *mode)
   return fd;
 }
 
+void fileclose(long fd)
+{
+  syscall1(SYS_close, fd);
+}
+
+// get a single character from the file you opened
+// returns it back in the ch buffer
+long getcharacter(long fd, char *ch)
+{
+  long bytes_read;
+  bytes_read = syscall3(SYS_read, fd, (long)ch, 1);
+  return bytes_read;
+}
+
+// simple printing to the screen
+// doesn't take extra arguments
 void print(char *string)
 {
   syscall3(SYS_write, 1, (long)string, strlen(string));
 }
 
+// prints one character to the screen
 void putcharacter(char *character)
 {
   syscall3(SYS_write, 1, (long)character, 1);
 }
 
+// main work
 int main(int argc, char *argv[])
 {
   if (argc != 2)
@@ -84,25 +110,31 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  char current_char;
+  char current_char = 1;
 
   // open file
-  long fd = fileopen(argv[1], "r");
+  long fd = fileopen(argv[1], O_RDONLY);
 
   if (fd < 0)
   {
     print("Error opening file\n");
     print(argv[1]);
     print("\n");
+    fileclose(fd);
     exit(2);
   }
 
-  // while ((current_char = fgetc(fptr)) != EOF)
-  // {
-  //   putcharacter(&current_char);
-  // }
+  long bytes_read;
+  do
+  {
+    bytes_read = getcharacter(fd, &current_char);
+    if (bytes_read > 0)
+    {
+      putcharacter(&current_char);
+    }
+  } while (bytes_read > 0);
 
-  // fclose(fptr);
+  fileclose(fd);
   return 0;
 }
 
